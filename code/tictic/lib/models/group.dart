fd84ec7822cc0f0b4de61b0d0301532870fcd922
Daniel Schreurs <daniel.schreurs@hotmail.com>
2023-08-19 22:00:59 +0200

    :sparkles: Introducing models

--- code/tictic/lib/models/group.dart
+++ code/tictic/lib/models/group.dart
@@ -8,11 +8,13 @@ class Group {
   final String? picturePath;
   final List<String>? tags;
   final DateTime startDate;
+  final User owner;
   final List<User> users;
   final List<Transaction>? transactions;
 
   const Group(
       {required this.title,
+      required this.owner,
       this.picturePath,
       this.tags,
       this.transactions,
@@ -25,6 +27,7 @@ List<Group> groups = [
     title: "💻 Code Magicians",
     picturePath: "assets/code.jpg",
     startDate: DateTime(2023, 8, 31),
+    owner: fictiveUsers[0],
     users: [fictiveUsers[0], fictiveUsers[1], fictiveUsers[2]],
     transactions: transactions.sublist(0, 5),
     tags: const ["Coding", "Magic", "Geeks"],
@@ -33,6 +36,7 @@ List<Group> groups = [
     title: "🌐 Web Weavers",
     picturePath: "assets/web.jpg",
     startDate: DateTime(2023, 9, 15),
+    owner: fictiveUsers[3],
     users: [fictiveUsers[3], fictiveUsers[4], fictiveUsers[5]],
     transactions: transactions.sublist(5, 10),
     tags: const ["Web Development", "Design", "Networking"],
@@ -41,6 +45,7 @@ List<Group> groups = [
     title: "🎮 Game Gurus",
     picturePath: "assets/game.jpg",
     startDate: DateTime(2023, 9, 30),
+    owner: fictiveUsers[0],
     users: [fictiveUsers[0], fictiveUsers[2], fictiveUsers[4]],
     transactions: transactions.sublist(10, 15),
   ),
